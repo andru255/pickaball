b1fa191f9f21b1107ccf6dda7f357e3e0c3b097b
Andrés Muñoz <anmunoz@belatrixsf.com>
2021-11-03 22:16:39 -0500

    feat: projection points

--- src/objects/arrow.object.ts
+++ src/objects/arrow.object.ts
@@ -1,10 +1,151 @@
-import { COLOR_PALETTE, GRID_UNIT } from "~/game.config";
+import { COLOR_PALETTE, config, GRID_UNIT, GROUND } from "~/game.config";
+import BallImageObject from "./ball.object";
+import CalculatorService, { Bound } from "~/services/calculator.service";
 
-export default class ArrowObject extends Phaser.GameObjects.Image {
+export default class ArrowObject {
+  private readonly stateSizes = {
+    normal: { width: GRID_UNIT, height: GRID_UNIT },
+    pressed: { width: GRID_UNIT * 2, height: GRID_UNIT * 2 },
+  };
+  private onLeaveEvt = (centerPos: Phaser.Math.Vector2) => {};
+  private onDragEvt = () => {};
+  private body?: Phaser.GameObjects.Group;
+  private box!: Phaser.GameObjects.Sprite;
+  private linkLine!: Phaser.Geom.Line;
+  private projection!: Phaser.Curves.Line;
+  private graphics!: Phaser.GameObjects.Graphics;
+  private initPos = { x: 0, y: 0 };
+  private calculador = new CalculatorService();
+
   constructor(scene: Phaser.Scene, x, y, texture) {
-    super(scene, x, y, texture);
-    this.setDisplaySize(GRID_UNIT, GRID_UNIT);
-    this.setTintFill(COLOR_PALETTE.lightBlue);
-    scene.children.add(this);
+    this.initPos.x = x;
+    this.initPos.y = y;
+    this.body = scene.add.group({
+      defaultKey: "arrowBody",
+      createCallback: () => {},
+    });
+
+    this.graphics = scene.add.graphics({
+      fillStyle: { color: COLOR_PALETTE.blue },
+      lineStyle: { color: COLOR_PALETTE.blue },
+    });
+
+    const { normal, pressed } = this.stateSizes;
+
+    // setup box
+    this.box = this.body.create(x, y);
+    this.box.setDisplaySize(normal.width, normal.height);
+    this.box.setTintFill(COLOR_PALETTE.darkBlue);
+    this.box.setInteractive();
+    scene.input.setDraggable(this.box);
+
+    this.box.on("pointerover", () => {
+      this.box.setDisplaySize(pressed.width, pressed.height);
+    });
+
+    this.box.on("pointerout", () => {
+      this.box.setDisplaySize(normal.width, normal.height);
+      const center = this.box.getCenter();
+      this.onLeaveEvt(center);
+    });
+
+    scene.input.on("drag", (pointer, gameObject, dragX, dragY) => {
+      this.box.setPosition(dragX, dragY);
+      this.linkLine.x1 = dragX;
+      this.linkLine.y1 = dragY;
+      this.onDragEvt();
+    });
+  }
+
+  public onLeave(callback = (centerPos: Phaser.Math.Vector2) => {}) {
+    this.onLeaveEvt = callback;
+  }
+
+  public onDrag(callback = () => {}) {
+    this.onDragEvt = callback;
+  }
+
+  public link(ball: BallImageObject) {
+    const { x: x1, y: y1 } = this.box.getCenter();
+    const { x: x2, y: y2 } = ball.getCenter();
+
+    this.linkLine = new Phaser.Geom.Line(x1, y1, x2, y2);
+    const dots = this.buildProjectionDots(ball);
+    this.projection = new Phaser.Curves.Line(dots);
+  }
+
+  public updateProjectionValues(ball: BallImageObject) {
+    const dots = this.buildProjectionDots(ball);
+    this.projection = new Phaser.Curves.Line(dots);
+  }
+
+  private getDistanceWithBall(ball: BallImageObject): number {
+    const { x: x1, y: y1 } = this.box.getCenter();
+    const { x: x2, y: y2 } = ball.getCenter();
+    return this.calculador.distanceAABB(
+      { x: x1, y: y1, width: 0, height: 0 },
+      { x: x2, y: y2, width: 0, height: 0 }
+    );
+  }
+
+  public hide() {
+    this.body?.setAlpha(0);
+    this.box.setAlpha(0);
+    this.graphics.alpha = 0;
+  }
+
+  public reset(ball: BallImageObject) {
+    this.body?.setAlpha(1);
+    this.box.setAlpha(1);
+    this.graphics.alpha = 1;
+    this.box.x = this.initPos.x;
+    this.box.y = this.initPos.y;
+    this.link(ball);
+  }
+
+  public renderLink() {
+    this.graphics.clear();
+    this.graphics.strokeLineShape(this.linkLine);
+    this.projection.getPoints().map((point) => {
+      this.graphics.fillRect(point.x, point.y, 5, 5);
+    });
+  }
+
+  private buildProjectionDots(ball: BallImageObject): number[] {
+    const result: number[] = [];
+
+    const { x: x1, y: y1 } = this.box.getCenter();
+    const { x: x2, y: y2 } = ball.getCenter();
+
+    // calculate the distance between the box and the ball
+    const distance = this.getDistanceWithBall(ball);
+    const angle = this.calculador.distanceAngle(
+      { x: x1, y: y1, width: 0, height: 0 },
+      { x: x2, y: y2, width: 0, height: 0 }
+    );
+
+    console.log("ball", x2, y2);
+    console.log("distance", distance);
+    console.log("angle", angle, (angle / Math.PI) * 180);
+
+    const speed = distance;
+    const accY = GROUND.GRAVITY;
+    const length = 10;
+    const fps = 1 / 3;
+    const steps = fps * length;
+    let vX = Math.cos(angle) * speed;
+    let vY = Math.sin(angle) * speed;
+    let x0 = x2;
+    let y0 = y2;
+
+    for (let i = 0; i < steps; i += fps) {
+      vY += accY;
+      x0 += fps * vX;
+      y0 += fps * vY;
+      result.push(x0);
+      result.push(y0);
+    }
+
+    return [result[length - 2], result[length - 1], result[0], result[1]];
   }
 }
